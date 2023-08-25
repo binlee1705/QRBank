7a390268f7088bc51749b3a8deeeab390b30376d
Lê Tuấn Anh <kunbin.jack@gmail.com>
2023-08-25 11:46:36 +0700

    a

--- javascript.js
+++ javascript.js
@@ -14,15 +14,17 @@ fetch('./data.json')
             const html = array.map((g, i) => (
                 `<div class="item wow fadeInDown" data-wow-delay="${(i * 0.2).toFixed(2)}s">
                     <div class="nameVertical">
-                        ${g.name}
+                        ${g.name.split(' ').join('<span class="space"></span>')}
                     </div>
                     <div class="bound">
                         <div class="image">
                             <div class="itImage">
                                 <img src="${g.image}" alt="${g.name}">
                             </div>
-                        </div>
-                       
+                        </div>                       
+                    </div> 
+                    <div class="birthday">
+                        ${g.birthday}
                     </div>
                 </div>`
             ));
