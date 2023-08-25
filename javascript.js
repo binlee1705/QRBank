ff265e66a7d2835b9ec4cbf4ac962960b1c1f175
Lê Tuấn Anh <kunbin.jack@gmail.com>
2023-08-25 11:03:59 +0700

    a

---
+++ javascript.js
@@ -0,0 +1,31 @@
+const wow = new WOW(
+    {
+        boxClass: 'wow',
+        animateClass: 'animated',
+        offset: 50,
+        live: true
+    }
+);
+new WOW().init();
+fetch('./data.json')
+    .then((response) => response.json())
+    .then((array) => {
+        if (array && array.length > 0) {
+            const html = array.map((g, i) => (
+                `<div class="item wow fadeInDown" data-wow-delay="${(i * 0.2).toFixed(2)}s">
+                    <div class="nameVertical">
+                        ${g.name}
+                    </div>
+                    <div class="bound">
+                        <div class="image">
+                            <div class="itImage">
+                                <img src="${g.image}" alt="${g.name}">
+                            </div>
+                        </div>
+                       
+                    </div>
+                </div>`
+            ));
+            $("#QR").html(html);
+        }
+    }); 
